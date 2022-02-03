ae7aeb26111a9cd58f40a51cf5fede27b4335d8a
Tiến anh Bùi <minhanhtienanh@gmail.com>
2022-02-03 16:20:27 +0700

    Add project files.

---
+++ AngularJSFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AngularJSFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
